1aa0a62ff92280384b4dde86ff28e0919da475e1
jwj123 <903107537@qq.com>
2018-05-11 22:29:47 +0800

    add server

---
+++ server/app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var ejs = require('ejs');
+
+var loginRoute=require('./routes/login');
+var homeRoute=require('./routes/home');
+var roleRoute=require('./routes/role');
+var editRoleRoute=require('./routes/edit_role');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',loginRoute);
+app.use('/home/',homeRoute);
+app.use('/role/',roleRoute);
+app.use('/edit_role/',editRoleRoute);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
